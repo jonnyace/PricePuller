956b63da111d06f040382c107b6e9ccfbfd5aa25
jonnyace <jonthn@gmail.com>
2014-03-25 14:52:28 -0500

    Create pricepuller.py

---
+++ pricepuller.py
@@ -0,0 +1,18 @@
+import urllib
+import re
+
+def get_quote(symbol):
+    base_url = 'http://finance.google.com/finance?q='
+    content = urllib.urlopen(base_url + symbol).read()
+    m = re.search('id="ref_694653_l".*?>(.*?)<', content)
+    if m:
+        quote = m.group(1)
+    else:
+        quote = 'no quote available for: ' + symbol
+    return quote
+
+print "Google", get_quote('goog') 
+
+from sys import argv
+
+print "Enter your stock ticker"
